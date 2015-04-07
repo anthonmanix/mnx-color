eed28c6e6441f208f4ef465fd7fb0917dce418c1
anthonmanix <aljosa.zagar@gmail.com>
2015-04-07 23:59:25 +0200

    rgb to ui

--- src/js/mnx-color.js
+++ src/js/mnx-color.js
@@ -5,76 +5,79 @@
       require: 'ngModel',
       link: function (scope, element, attrs, ctrl) {
         var
-          h = 0, s = 0, v = 1, a = 0, current,
+          rgb = [255, 255, 255], hsv = [0, 0, 1], a = 1, pickers, current,
           container = angular.element('<div class="cp-container"></div>'),
-          color = angular.element('<div class="cp-col"></div>'),
-          colorsvg = [
-            '<svg width=100% height=100% xmlns=http://www.w3.org/2000/svg>',
-              '<defs>',
-                '<linearGradient id=s>',
-                  '<stop offset=0 stop-color=#fff/>',
-                  '<stop offset=1 stop-color=#fff stop-opacity=0/>',
-                '</linearGradient>',
-                '<linearGradient id=v x2=0 y2=1>',
-                  '<stop offset=0 stop-color=#000 stop-opacity=0/>',
-                  '<stop offset=1 stop-color=#000/>',
-                '</linearGradient>',
-              '</defs>',
-              '<rect width=100% height=100% fill=url(#s)/>',
-              '<rect width=100% height=100% fill=url(#v)/>',
-            '</svg>'
-          ].join(''),
-          hue = angular.element('<div class="cp-hue"></div>'),
-          huesvg = [
-            '<svg width=100% height=100% xmlns=http://www.w3.org/2000/svg>',
-              '<defs>',
-                '<linearGradient id=h x2=0 y2=1>',
-                  '<stop stop-color=#f00 offset=0/>',
-                  '<stop stop-color=#ff0 offset=.17/>',
-                  '<stop stop-color=#0f0 offset=.33/>',
-                  '<stop stop-color=#0ff offset=.5/>',
-                  '<stop stop-color=#00f offset=.67/>',
-                  '<stop stop-color=#f0f offset=.83/>',
-                  '<stop stop-color=#f00 offset=1/>',
-                '</linearGradient>',
-              '</defs>',
-              '<rect width=100% height=100% fill=url(#h)/>',
-            '</svg>'
-          ].join(''),
-          alpha = angular.element('<div class="cp-alpha"></div>'),
-          alphasvg = [
-            '<svg width=100% height=100% xmlns=http://www.w3.org/2000/svg>',
-              '<defs>',
-                '<linearGradient id=a>',
-                  '<stop offset=0 stop-opacity=0 stop-color=#fff/>',
-                  '<stop offset=1 stop-color=#fff/>',
-                '</linearGradient>',
-              '</defs>',
-              '<rect width=100% height=100% fill=url(#a)/>',
-            '</svg>'
-          ].join(''),
+          color = angular.element([
+            '<div name="col" class="cp-col">',
+              '<svg width=100% height=100% xmlns=http://www.w3.org/2000/svg>',
+                '<defs>',
+                  '<linearGradient id=s>',
+                    '<stop offset=0 stop-color=#fff/>',
+                    '<stop offset=1 stop-color=#fff stop-opacity=0/>',
+                  '</linearGradient>',
+                  '<linearGradient id=v x2=0 y2=1>',
+                    '<stop offset=0 stop-color=#000 stop-opacity=0/>',
+                    '<stop offset=1 stop-color=#000/>',
+                  '</linearGradient>',
+                '</defs>',
+                '<rect width=100% height=100% fill=url(#s)/>',
+                '<rect width=100% height=100% fill=url(#v)/>',
+              '</svg>',
+            '</div>'
+          ].join('')),
+          hue = angular.element([
+            '<div name="hue" class="cp-hue">',
+              '<svg width=100% height=100% xmlns=http://www.w3.org/2000/svg>',
+                '<defs>',
+                  '<linearGradient id=h x2=0 y2=1>',
+                    '<stop stop-color=#f00 offset=0/>',
+                    '<stop stop-color=#ff0 offset=.17/>',
+                    '<stop stop-color=#0f0 offset=.33/>',
+                    '<stop stop-color=#0ff offset=.5/>',
+                    '<stop stop-color=#00f offset=.67/>',
+                    '<stop stop-color=#f0f offset=.83/>',
+                    '<stop stop-color=#f00 offset=1/>',
+                  '</linearGradient>',
+                '</defs>',
+                '<rect width=100% height=100% fill=url(#h)/>',
+              '</svg>',
+            '</div>'
+          ].join('')),
+          alpha = angular.element([
+            '<div name="alpha" class="cp-alpha">',
+              '<svg width=100% height=100% xmlns=http://www.w3.org/2000/svg>',
+                '<defs>',
+                  '<linearGradient id=a>',
+                    '<stop offset=0 stop-opacity=0 stop-color=#fff/>',
+                    '<stop offset=1 stop-color=#fff/>',
+                  '</linearGradient>',
+                '</defs>',
+                '<rect width=100% height=100% fill=url(#a)/>',
+              '</svg>',
+            '</div>'
+          ].join('')),
           ccursor = angular.element('<div class="cp-ccursor"></div>'),
           hcursor = angular.element('<div class="cp-hcursor"></div>'),
           acursor = angular.element('<div class="cp-acursor"></div>');
 
         container.append(color).append(hue).append(alpha);
-        color.append(colorsvg).append(ccursor).on('mousedown', mousedown);
-        hue.append(huesvg).append(hcursor).on('mousedown', mousedown);
-        alpha.append(alphasvg).append(acursor).on('mousedown', mousedown);
+        color.append(ccursor).on('mousedown', mousedown);
+        hue.append(hcursor).on('mousedown', mousedown);
+        alpha.append(acursor).on('mousedown', mousedown);
         element.append(container);
+        pickers = { col: pickerMeta(color), hue: pickerMeta(hue), alpha: pickerMeta(alpha) };
+        inputUpdate();
 
-        ccursor.css({ top: 0, left: 0 });
-        hcursor.css({ top: 0, left: 0 });
-        acursor.css({ top: 0, left: 0 });
+        function pickerMeta(picker) {
+          return {
+            name: picker.attr('name'),
+            width: picker.prop('clientWidth') - 1, height: picker.prop('clientHeight') - 1,
+            top: picker.prop('offsetTop'), left: picker.prop('offsetLeft')
+          };
+        }
 
         function mousedown(event) {
-          current = {
-            name: angular.element(this).prop('className'),
-            width: angular.element(this).prop('clientWidth') - 1,
-            height: angular.element(this).prop('clientHeight') - 1,
-            top: angular.element(this).prop('offsetTop'),
-            left: angular.element(this).prop('offsetLeft')
-          }
+          current = pickers[angular.element(this).attr('name')];
           mousemove(event);
           $document.on('mousemove', mousemove).on('mouseup', function mouseup() {
             $document.off('mousemove', mousemove).off('mouseup', mouseup);
@@ -87,48 +90,86 @@
             y = Math.min(current.height, Math.max(0, event.pageY - current.top));
           event.stopPropagation();
           event.preventDefault();
-          setColor(x, y);
+          pickerUpdate(x, y);
         }
 
-        function setColor(x, y) {
-          if (current.name === 'cp-col') {
-            s = bound(x, current.width);
-            v = bound(current.height - y, current.height);
-            ccursor.css({ top: y + 'px', left: x + 'px' });
-          } else if (current.name === 'cp-hue') {
-            h = bound(y, current.height);
-            hcursor.css({ top: y + 'px' });
-            color.css({ 'background-color': 'rgb(' + hsvToRgb(h, 1, 1).join(',') + ')' });
-          } else if (current.name === 'cp-alpha') {
+        function pickerUpdate(x, y) {
+          if (current.name === 'alpha') {
             a = bound(x, current.width);
             acursor.css({ left: x + 'px' });
+          } else {
+            if (current.name === 'col') {
+              hsv[1] = bound(x, current.width);
+              hsv[2] = bound(current.height - y, current.height);
+              ccursor.css({ top: y + 'px', left: x + 'px' });
+            } else if (current.name === 'hue') {
+              hsv[0] = bound(y, current.height);
+              hcursor.css({ top: y + 'px' });
+              color.css({ 'background-color': 'rgb(' + hsvToRgb([hsv[0], 1, 1]) + ')' });
+            }
+            rgb = hsvToRgb(hsv);
+            alpha.find('stop').attr('stop-color', 'rgb(' + rgb + ')');
           }
-          alpha.find('stop').attr('stop-color', 'rgb(' + hsvToRgb(h, s, v) + ')');
-          ctrl.$setViewValue('rgba(' + hsvToRgb(h, s, v).join(',') + ',' + Math.round(a * 100) / 100 + ')');
+          ctrl.$setViewValue('rgba(' + rgb + ',' + Math.round(a * 100) / 100 + ')');
+        }
+
+        function inputUpdate() {
+          hsv = rgbToHsv(rgb);
+          ccursor.css({
+            top: Math.round(pickers.col.height - hsv[2] * pickers.col.height) + 'px',
+            left: Math.round(hsv[1] * pickers.col.width) + 'px'
+          });
+          hcursor.css({ top: Math.round(hsv[0] * pickers.hue.height) + 'px', left: 0 });
+          acursor.css({ top: 0, left: Math.round(a * pickers.alpha.width) + 'px' });
+          color.css({ 'background-color': 'rgb(' + hsvToRgb([hsv[0], 1, 1]) + ')' });
+          alpha.find('stop').attr('stop-color', 'rgb(' + rgb + ')');
         }
       }
     };
-
-    function bound(n, max) {
-      if (Math.abs(n - max) < 0.000001) return 1;
-      return (n % max) / parseFloat(max);
-    }
-
-    function hsvToRgb(h, s, v) {
-      h = h * 6;
-      var
-        i = Math.floor(h),
-        f = h - i,
-        p = v * (1 - s),
-        q = v * (1 - s * f),
-        t = v * (1 - s * (1 - f)),
-        mod = i % 6,
-        r = [v, q, p, p, t, v][mod],
-        g = [t, v, v, q, p, p][mod],
-        b = [p, p, t, v, v, q][mod];
-      return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-    }
   }
   MnxColor.$inject = ['$document'];
+
+  function bound(n, max) {
+    if (Math.abs(n - max) < 0.000001) return 1;
+    return (n % max) / parseFloat(max);
+  }
+
+  function rgbToHsv(rgb) {
+    var
+      r = bound(rgb[0], 255),
+      g = bound(rgb[1], 255),
+      b = bound(rgb[2], 255),
+      max = Math.max(r, g, b), min = Math.min(r, g, b),
+      h, s, v = max,
+      d = max - min;
+    s = max === 0 ? 0 : d / max;
+    if (max == min) {
+        h = 0;
+    } else {
+      switch (max) {
+        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+        case g: h = (b - r) / d + 2; break;
+        case b: h = (r - g) / d + 4; break;
+      }
+      h /= 6;
+    }
+    return [h, s, v];
+  }
+
+  function hsvToRgb(hsv) {
+    var
+      h = hsv[0] * 6, s = hsv[1], v = hsv[2],
+      i = Math.floor(h),
+      f = h - i,
+      p = v * (1 - s),
+      q = v * (1 - s * f),
+      t = v * (1 - s * (1 - f)),
+      mod = i % 6,
+      r = [v, q, p, p, t, v][mod],
+      g = [t, v, v, q, p, p][mod],
+      b = [p, p, t, v, v, q][mod];
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+  }
+
   angular.module('mnxColor', []).directive('mnxColor', MnxColor);
 })();
